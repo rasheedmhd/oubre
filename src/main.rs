7788d5277d7a0ab653f2538282810f531bde40a3
Virux <starletgh@gmail.com>
2023-01-09 14:48:19 +0000

    A Minimal Rust Kernel

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,23 @@ use core::panic::PanicInfo;
 // design to ensure that the compiler has unique names to
 // variable bindings, function names etc
 //https://en.wikipedia.org/wiki/Name_mangling
-#[no_mangle]
 
+static HELLO: &[u8] = b"Hello World!, I am Oubre, an Operating System created by Starlet ;)";
 // extern "C" tells the compiler to use the C calling convention.
 // Calling conventions are a low level implementation of how functions
 // should receive parameters from calling functions and how to return the results.
 // https://en.wikipedia.org/wiki/Calling_convention
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
