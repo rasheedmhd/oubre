2aedad0fc5be5dee5a457c838f013da144ba9ac6
Virux <starletgh@gmail.com>
2023-03-23 17:29:40 +0000

    translating scancodes of numbers

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -101,8 +101,24 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(stack_frame: InterruptStack
 {
     use x86_64::instructions::port::Port;
     let mut port = Port::new(0x60);
-    let scan_code: u8 = unsafe { port.read() };
-    println!("{}", scan_code);
+    let scancode: u8 = unsafe { port.read() };
+
+    let key = match scancode {
+        0x02 => Some('1'),
+        0x03 => Some('2'),
+        0x04 => Some('3'),
+        0x05 => Some('4'),
+        0x06 => Some('5'),
+        0x07 => Some('6'),
+        0x08 => Some('7'),
+        0x09 => Some('8'),
+        0x0a => Some('9'),
+        0x0b => Some('0'),
+        _ => None,
+    };
+    if let Some(key) = key {
+        println!("{}", key);
+    }
 
     unsafe {
         PICS.lock()
