1f91d664b19f2e8cef73cc185529ee5350cdeb70
Virux <starletgh@gmail.com>
2023-03-25 16:41:54 +0000

    hardware Interrupts completed

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -8,6 +8,16 @@ use x86_64::structures::idt::{
     InterruptDescriptorTable, 
     InterruptStackFrame
 };
+//------------------------------------ keyboard_interrupt_handler imports
+use x86_64::instructions::port::Port; 
+use pc_keyboard::{
+    layouts, 
+    DecodedKey, 
+    HandleControl, 
+    Keyboard,
+    ScancodeSet1
+};
+//------------------------------------ end of keyboard_interrupt_handler
 
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
@@ -99,25 +109,32 @@ extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: InterruptStackFr
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(stack_frame: InterruptStackFrame) 
 {
-    use x86_64::instructions::port::Port;
-    let mut port = Port::new(0x60);
-    let scancode: u8 = unsafe { port.read() };
 
-    let key = match scancode {
-        0x02 => Some('1'),
-        0x03 => Some('2'),
-        0x04 => Some('3'),
-        0x05 => Some('4'),
-        0x06 => Some('5'),
-        0x07 => Some('6'),
-        0x08 => Some('7'),
-        0x09 => Some('8'),
-        0x0a => Some('9'),
-        0x0b => Some('0'),
-        _ => None,
-    };
-    if let Some(key) = key {
-        println!("{}", key);
+    // lazy_static! {
+    //     static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = 
+    //         Mutex::new(
+    //             Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore)
+    //         )
+    // }
+
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = {
+            let keyboard = Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore));
+            keyboard
+        };
+    }
+
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode) {
+        if let Some(key) = keyboard.process_keyevent(key_event) {
+            match key {
+                DecodedKey::Unicode(character) => print!("{}", character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
     }
 
     unsafe {
