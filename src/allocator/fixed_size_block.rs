13b1235ab147fd502159610430f24fd14f238775
Virux <starletgh@gmail.com>
2023-05-10 14:12:18 +0000

    adding a private fallback allocator

--- src/allocator/fixed_size_block.rs
+++ src/allocator/fixed_size_block.rs
@@ -1,12 +1,21 @@
+use alloc::alloc::{
+    Layout,
+    GlobalAlloc,
+};
+
+use core::ptr::null_mut;
+
 struct ListNode {
-    // all nodes have thesame fixed size
+    // all nodes have the same fixed size
     next: Option<&'static mut ListNode>
 }
 
 
+
+
 /// block sizes
 /// must be a power of 2 to help with alignments (size alignments must be powers of 2)
-const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128,256, 5112, 1024, 2048];
+const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128,256, 512, 1024, 2048];
 
 /// FSB -> [F]ixed[S]ized[B]lock 
 pub struct FSBAllocator {
@@ -26,4 +35,11 @@ impl FSBAllocator {
     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
         self.fallback_allocator.init(heap_start, heap_size);
     } 
+
+    fn fallback_alloc(&mut self, layout: Layout) -> *mut u8 {
+        match self.fallback_allocator.allocate_first_fit(layout) {
+            Ok(ptr) => ptr.as_ptr(),
+            Err(_) => null_mut(),
+        }
+    }
 }
