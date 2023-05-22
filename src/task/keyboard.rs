7052b43b2c79f624de4eac1ea51325711ba16101
Virux <starletgh@gmail.com>
2023-05-22 17:47:14 +0000

    Implementing Stream

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -2,6 +2,15 @@
 use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
 use crate::println;
+use futures_util::stream::Stream;
+
+use core::{
+    pin::Pin,
+    task::{
+        Poll,
+        Context,
+    }
+};
 
 // OnceCell, a cell type with interior mutability, that offers a read only access to the value
 // that it wraps once initialized. It can only be written to once. 
@@ -16,5 +25,32 @@ pub(crate) fn add_scancode(scancode: u8) {
         }
     } else {
         println!("WARNING: scancode queue uninitialized");
+    }
+}
+
+pub struct ScancodeStream {
+    _private: (),
+}
+
+impl ScancodeStream {
+    pub fn new() -> Self {
+        // try init the SCANCODE_QUEUE panic if it has been init already.
+        SCANCODE_QUEUE.try_init_once(|| ArrayQueue::new(100))
+        .expect("ScancodeStream::new should only be called once");
+        ScancodeStream {
+            _private: ()
+        }
+    }
+}
+
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE.try_get().expect("not initialized");
+        match queue.pop() {
+            Ok(scancode) => Poll::Ready(Some(scancode)),
+            Err(crossbeam_queue::PopError) => Poll::Pending,
+        }
     }
 }
